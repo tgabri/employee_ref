3dfb17babcd81c8ab1a410cc9f946e6858a499d4
Gabor <tgabri@gmail.com>
2019-11-29 17:07:50 +0000

    basic server.js is created, deendencies installed

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(express.json());
+
+//DB Config
+
+const db = require('./config/keys').mongoURI;
+
+//Connect to mongoDB
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
